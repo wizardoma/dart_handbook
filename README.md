5c59b5ec1ce9e2d897f90598db758164db554faf
Ibekason Alexander <alibekason@gmail.com>
2021-06-18 17:37:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dart Handbook
+
+A mobile application handbook for the dart programming language. 
+
+
---
+++ android/app/src/main/java/com/wizardom/dart_handbook/MainActivity.java
@@ -0,0 +1,6 @@
+package com.wizardom.dart_handbook;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
